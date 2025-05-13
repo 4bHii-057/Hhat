6d92809233e580fbf0e9ff4d554a32e34715b0db
Abhi Jhirwal <am3276216@gmail.com>
2025-05-14 00:02:11 +0530

    Update run_payal.py

--- run_payal.py
+++ run_payal.py
@@ -1,7 +1,1 @@
 import payal
-
-if hasattr(payal, "main"):
-    payal.main()
-else:
-    print("[!] Error: 'main' function not found in payal module.")
-    print("Available functions/attributes:", dir(payal))
