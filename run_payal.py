50093278cfed58e8d17db9cc3a292762fec02ab1
Abhi Jhirwal <am3276216@gmail.com>
2025-05-13 23:30:13 +0530

    Update run_payal.py

--- run_payal.py
+++ run_payal.py
@@ -1,3 +1,10 @@
+import sys
+import traceback
 
-import payal
+try:
+    import payal
+except Exception as e:
+    print("[!] Error importing .so:", e)
+    traceback.print_exc()
+    sys.exit(1)
 # Your encrypted module is now running.
